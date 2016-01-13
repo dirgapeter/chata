e6e297795272b9627c64c5f5b627c66866138e90
Peter Dirga <dirga123@gmail.com>
2016-01-13 14:16:02 +0100

    polyfill findIndex

--- app/main.jsx
+++ app/main.jsx
@@ -27,4 +27,27 @@ const routerInstance = (
   </Router>
 );
 
+if (!Array.prototype.findIndex) {
+  Array.prototype.findIndex = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
 ReactDOM.render(routerInstance, document.getElementById('app'));
